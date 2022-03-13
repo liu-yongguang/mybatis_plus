ca2f2cefe573dcce5fb7bf9ec11d2fa1d1bd7f52
liuyongguang <18317837185@163.com>
2022-03-13 17:06:13 +0800

    test 第一次push

---
+++ src/test/java/com/kuang/TestPush.java
@@ -0,0 +1,14 @@
+package com.kuang;
+
+/**
+ * @program: mybatis_plus
+ * @description:
+ * @author:Mr.Liu
+ * @CreateDate: 2022/3/13 17:04
+ * @version: 1.0
+ */
+public class TestPush {
+    public static void main(String[] args) {
+        System.out.println("第一次push！");
+    }
+}
