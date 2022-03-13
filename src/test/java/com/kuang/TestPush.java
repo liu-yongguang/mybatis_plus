f769964590106936c4a9d427eec0236c3b6a6378
liuyongguang <18317837185@163.com>
2022-03-13 17:09:01 +0800

    test 第2次push

--- src/test/java/com/kuang/TestPush.java
+++ src/test/java/com/kuang/TestPush.java
@@ -10,5 +10,6 @@ package com.kuang;
 public class TestPush {
     public static void main(String[] args) {
         System.out.println("第一次push！");
+        System.out.println("第2次push！");
     }
 }
