323a284a128df64cbac47b48bd08d626bf9b341d
liuyongguang <18317837185@163.com>
2022-03-13 17:13:45 +0800

    test 第3次push

--- src/test/java/com/kuang/TestPush.java
+++ src/test/java/com/kuang/TestPush.java
@@ -11,5 +11,6 @@ public class TestPush {
     public static void main(String[] args) {
         System.out.println("第一次push！");
         System.out.println("第2次push！");
+        System.out.println("第3次push！");
     }
 }
